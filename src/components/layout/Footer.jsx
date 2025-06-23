aa05db57353bd2f0bce0f33cc1f176aeccb4cfd4
Mukul Joshi <145245946+HeyMeMukul@users.noreply.github.com>
2025-06-23 19:29:26 +0530

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -26,12 +26,7 @@ const Footer = () => {
               3D cybersecurity learning platform with interactive tools and gamified education.
             </p>
             
-            {/* Contact Links */}
-            <div className="flex space-x-3">
-              <SocialLink href="https://github.com" icon={<Github className="w-4 h-4" />} label="GitHub" />
-              <SocialLink href="mailto:contact@cyberverse.hq" icon={<Mail className="w-4 h-4" />} label="Email" />
-            </div>
-          </div>
+       
 
           {/* Navigation Links */}
           <div>
